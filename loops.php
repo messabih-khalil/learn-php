0a27839ec32f8ca7e21c0b034b74f4fd05185903
messabih-khalil <msbih.khalil@gmail.com>
2023-03-08 22:26:37 -0800

    loops

---
+++ loops.php
@@ -0,0 +1,19 @@
+<?php
+
+    $users = [
+        [
+            "username" => "aladin",
+            "age" => "99"
+        ],
+        [
+            "username" => "khalil",
+            "age" => "98"
+        ],
+    ];
+
+    
+    foreach($users as $user){
+        echo $user;
+    }
+
+?>
